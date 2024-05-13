9ecaa2ef372dc0913b97a63e8556ebd0465f5916
Blueboltz <vincentmariscotescleofe@gmail.com>
2024-05-14 01:05:17 +0800

    added navbar, hero, and footer in index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   {{-- CSS LINK --}}
+   <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/navigation.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+
+   {{-- GOOGLE FONTS LINK --}}
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+
+   {{-- FONTAWESOME CDN --}}
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+   <title>Home | SoleAce</title>
+</head>
+<body>
+   {{-- NAVIGATION --}}
+   @include('partials._navigation')
+   
+   <main>
+      {{-- HERO --}}
+      <section class="hero">
+         <h1 class="hero--slogan">Unleash Your Sole<br> Power With<br> <span>SoleAce</span></h1>
+         <img class="hero--shoe-item" src="{{asset('images/hero-shoe-item.png')}}" alt="A picture of a big shoe">
+      </section>
+
+      {{-- FEATURED PRODUCTS --}}
+   </main>
+
+   {{-- FOOTER --}}
+   @include('partials._footer')
+</body>
+</html>
---
+++ resources/views/partials/_navigation.blade.php
@@ -0,0 +1,57 @@
+    <nav>
+         <a href="/" class="business-logo-container">
+            <img class="business-logo" src="{{asset('images/business-logo.png')}}" alt="An image of a business logo">
+         </a>
+
+         <ul class="categories">
+            <li>
+               New & Featured
+
+               <ul class="dropdown-menu">
+                  <li><a href="">New Arrivals</a></li>
+                  <li><a href="">Featured Shoes</a></li>
+               </ul>
+            </li>
+
+            <li>
+               Men
+
+               <ul class="dropdown-menu">
+                  <li><a href="">All Shoes For Men</a></li>
+                  <li><a href="">Casual</a></li>
+                  <li><a href="">Running</a></li>
+                  <li><a href="">Sneakers</a></li>
+               </ul>
+            </li>
+
+            <li>
+               Women
+
+               <ul class="dropdown-menu">
+                  <li><a href="">All Shoes For Women</a></li>
+                  <li><a href="">Casual</a></li>
+                  <li><a href="">Running</a></li>
+               </ul>
+            </li>
+
+            
+            <li>
+               Kids
+
+               <ul class="dropdown-menu">
+                  <li><a href="">All Shoes For Kids</a></li>
+                  <li><a href="">Boys</a></li>
+                  <li><a href="">Girls</a></li>
+               </ul>
+            </li>
+         </ul>
+      
+         <a class="sign-in-link" href="sign-in.php">
+            <h3>Sign in</h3>
+         </a>
+
+         <a href="" class="shopping-cart-link" id="shopping-cart-link">
+            <i class="fa-solid fa-cart-shopping shopping-cart"></i>
+         </a>
+
+      </nav>
